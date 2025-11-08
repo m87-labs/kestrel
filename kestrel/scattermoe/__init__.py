535288b771e7264cc6c0cd8970cda7f85fd9eefd
Vikhyat Korrapati <vikhyatk@gmail.com>
2025-11-07 18:48:30 -0800

    switch over to the fused MoE kernel completely

--- kestrel/scattermoe/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Scatter MoE accelerated kernels and modules."""
-
-from .mlp import MLP
-from .parallel_experts import ParallelExperts, parallel_linear
-from .workspace import ScatterMoEWorkspace
-
-__all__ = ["MLP", "ParallelExperts", "parallel_linear", "ScatterMoEWorkspace"]
