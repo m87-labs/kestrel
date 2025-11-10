07aa1d9c0b88cc2bf39d22184bb59aff7d88f4ba
Vikhyat Korrapati <vikhyatk@gmail.com>
2025-11-10 01:06:11 -0800

    even faster kv cache updates

--- kestrel/ops/__init__.py
+++ kestrel/ops/__init__.py
@@ -1,10 +1,5 @@
 """Low-level Triton ops for Kestrel."""
 
-from .reshape_and_cache import reshape_and_cache_hnd
 from .rope import apply_rotary_emb, precompute_freqs_cis
 
-__all__ = [
-    "apply_rotary_emb",
-    "precompute_freqs_cis",
-    "reshape_and_cache_hnd",
-]
+__all__ = ["apply_rotary_emb", "precompute_freqs_cis"]
